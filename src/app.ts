e16a400a194343f3fca5e6316702db9297c18c2d
kushal tamang <kushaltamang0926@gmail.com>
2025-08-31 17:07:50 +0545

    G: implemmenting error handlinding through middleware

--- src/app.ts
+++ src/app.ts
@@ -4,26 +4,41 @@ import { PORT } from "./config/env.ts";
 import authRoutes from "./routes/auth.routes.ts";
 import userRoutes from "./routes/user.routes.ts";
 import subscriptionRoutes from "./routes/subscription.routes.ts";
-import connectDatabase from "./database/monogdb.ts";
+import connectDatabase from "./database/mongodb.ts";
+import errorMiddlware from "./middleware/error.middleware.ts";
 
 const app: Express = express();
 
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/subscription", subscriptionRoutes);
 
-
+// error handler
+app.use(errorMiddlware);
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`making the subscription tracking app and something olay`);
 });
 
-app.listen(PORT, async () => {
-  console.log(`the server is running in the http://localhost:${PORT}`);
 
-  await connectDatabase()
+const startServer = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`the server is running in the http://localhost:${PORT}`);
+    });
+    await connectDatabase();
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
 
-});
+startServer();
 
 export default app;
 
---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,44 @@
+import  type { Request, Response, NextFunction } from "express";
+
+type CustomError = Error & {
+    statusCode?: number;
+    code?: number;
+    errors?: Record<string, { message: string }>;
+    name?: string;
+}
+
+const errorMiddlware = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
+    try {
+        let error = { ...err }
+        error.message = err.message;
+        console.error(err);
+
+        // mongoose bad objecytID
+        if (error.name === "CastError") {
+            const message = "Resource not found";
+            error = new Error(message);
+            error.statusCode = 404;
+        }
+
+        // mongoose duplicate key
+        if (err.code === 11000) {
+            const message = "Duplicte field value entered";
+            error = new Error(message);
+            error.statusCode = 400;
+        }
+
+        // mongoose validation error
+        if (error.name === "ValidationError" && err.errors) {
+            const message = Object.values(err.errors).map(val => val.message);
+            error = new Error(message.join(", "));
+            error.statusCode = 400;
+        }
+
+        res.status(error.statusCode || 500).json({ success: false, error: error.message || "Server Error" });
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+export default errorMiddlware;
